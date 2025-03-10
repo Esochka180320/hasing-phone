efd1a5a6475ec2588a90908c54df61a5819812b3
Vlad Lutsenko <vladik3641@gmail.com>
2025-03-10 19:06:41 +0200

    final

--- src/main/java/com/vilka/hashing_service/validation/Validation.java
+++ src/main/java/com/vilka/hashing_service/validation/Validation.java
@@ -9,11 +9,11 @@ public class Validation {
     private static final Pattern PHONE_PATTERN = Pattern.compile("^380\\d{9}$");
 
     public boolean isNotValidPhoneNumber(String phoneNumber) {
-        return !PHONE_PATTERN.matcher(phoneNumber).matches() && isNull(phoneNumber);
+        return !PHONE_PATTERN.matcher(phoneNumber).matches() || isNull(phoneNumber);
     }
 
     public boolean isNull(String phoneNumberOrHash) {
-        return phoneNumberOrHash == null && phoneNumberOrHash.isEmpty();
+        return phoneNumberOrHash == null || phoneNumberOrHash.isEmpty();
     }
 
 }
